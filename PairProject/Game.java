8dce79967c2c345b590d8a0cf75f3b63b0b4fa1d
Darry Lu <36702191+ldu0893@users.noreply.github.com>
2020-04-28 16:16:14 -0400

    Update Game.java

--- PairProject/Game.java
+++ PairProject/Game.java
@@ -23,7 +23,7 @@ public class Game extends JPanel {
 		//meshList.add(new MeshCube());
 		try {
 			System.out.println("a");
-			meshList.add(Mesh.loadFromObjFile("Models/VideoShip.obj"));
+			meshList.add(Mesh.loadFromObjFile("Models/CubeTest.obj"));
 			System.out.println("b");
 		} catch (Exception e) {}
 		
