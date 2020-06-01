7401a3fa5a37a26249d3cfbcce4a361fdaea5722
Darry Lu <36702191+ldu0893@users.noreply.github.com>
2020-05-31 21:07:46 -0400

    Added another enemy

--- PairProject/EnemyA.java
+++ PairProject/EnemyA.java
@@ -6,7 +6,7 @@ public class EnemyA extends SpaceShip {
 		super();
 		setPos(pos);
 		try {
-			setTris(Mesh.loadFromObjFile("Models/Explosion.obj", "Textures/Explosion Map 1.png").getTris());
+			setTris(Mesh.loadFromObjFile("Models/Ship Model 4.obj", "Textures/Ship Model 4 Map.png").getTris());
 		} catch (Exception e) { System.out.println("Error loading smallbulletsphere2.obj"); }
 		translate(pos);
 		
