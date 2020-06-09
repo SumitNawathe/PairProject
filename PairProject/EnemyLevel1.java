410b1d76c6c3aea09e45a917bdff2f7b22e3f66a
Darry Lu <36702191+ldu0893@users.noreply.github.com>
2020-06-09 15:07:21 -0400

    Started the enemy levels

---
+++ PairProject/EnemyLevel1.java
@@ -0,0 +1,23 @@
+import java.awt.Graphics;
+
+public class EnemyLevel1 extends Level {
+
+	@Override
+	public void initializeGame(Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean update(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void draw(Game game, Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
